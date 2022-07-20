5a1c4a2901d2c53e8f9d7c25f88f73c2a7f17875
ChristianChiarulli <chris.machine@pm.me>
2022-07-19 21:50:21 -0400

    formatting

--- lua/lsp-inlayhints/core.lua
+++ lua/lsp-inlayhints/core.lua
@@ -1,300 +1,302 @@
 -- https://github.com/simrat39/rust-tools.nvim
 local M = {}
-local utils = require("lsp-inlayhints.utils")
-local config = require("lsp-inlayhints.config")
+local utils = require "lsp-inlayhints.utils"
+local config = require "lsp-inlayhints.config"
 
 function M.setup_autocmd(bufnr, endpoint)
-	local events = "BufEnter,BufWinEnter,TabEnter,BufWritePost,CursorHold,InsertLeave"
-	if config.options.tools.inlay_hints.only_current_line then
-		events = string.format("%s,%s", events, config.options.tools.inlay_hints.only_current_line_autocmd)
-	end
-	local group = vim.api.nvim_create_augroup("InlayHints", {})
-	vim.api.nvim_create_autocmd(vim.split(events, ","), {
-		group = group,
-		buffer = bufnr,
-		callback = function()
-			require("lsp-inlayhints").inlay_hints(endpoint)
-		end,
-	})
+  local events = "BufEnter,BufWinEnter,TabEnter,BufWritePost,CursorHold,InsertLeave"
+  if config.options.tools.inlay_hints.only_current_line then
+    events =
+      string.format("%s,%s", events, config.options.tools.inlay_hints.only_current_line_autocmd)
+  end
+  local group = vim.api.nvim_create_augroup("InlayHints", {})
+  vim.api.nvim_create_autocmd(vim.split(events, ","), {
+    group = group,
+    buffer = bufnr,
+    callback = function()
+      require("lsp-inlayhints").inlay_hints(endpoint)
+    end,
+  })
 end
 
 --- Return visible range of the buffer
 local function get_visible_range()
-	local _, line1, col1 = unpack(vim.fn.getpos("w0"))
-	local _, line2, col2 = unpack(vim.fn.getpos("w$"))
+  local _, line1, col1 = unpack(vim.fn.getpos "w0")
+  local _, line2, col2 = unpack(vim.fn.getpos "w$")
 
-	return { start = { line1, col1 }, _end = { line2, col2 } }
+  return { start = { line1, col1 }, _end = { line2, col2 } }
 end
 
 local function get_hint_ranges()
-	local line_count = vim.api.nvim_buf_line_count(0)
+  local line_count = vim.api.nvim_buf_line_count(0)
 
-	if line_count <= 200 then
-		return {
-			start = { 0, 0 },
-			_end = { line_count, vim.fn.col({ line_count, "$" }) },
-		}
-	end
+  if line_count <= 200 then
+    return {
+      start = { 0, 0 },
+      _end = { line_count, vim.fn.col { line_count, "$" } },
+    }
+  end
 
-	local extra = 30
-	local range = get_visible_range()
+  local extra = 30
+  local range = get_visible_range()
 
-	local start = math.max(1, range.start[1] - extra)
-	if range.start ~= start then
-		range.start = { start, vim.fn.col({ start, "$" }) }
-	end
+  local start = math.max(1, range.start[1] - extra)
+  if range.start ~= start then
+    range.start = { start, vim.fn.col { start, "$" } }
+  end
 
-	local _end = math.min(line_count, range._end[1] + extra)
-	if range._end ~= _end then
-		range._end = { _end, vim.fn.col({ _end, "$" }) }
-	end
+  local _end = math.min(line_count, range._end[1] + extra)
+  if range._end ~= _end then
+    range._end = { _end, vim.fn.col { _end, "$" } }
+  end
 
-	return range
+  return range
 end
 
 ---@param bufnr number
 -- TODO make_given_range_params accepts offset_encoding, we should pass it
 local function get_params(bufnr)
-	local range = get_hint_ranges()
-	local params = vim.lsp.util.make_given_range_params(range.start, range._end, bufnr)
+  local range = get_hint_ranges()
+  local params = vim.lsp.util.make_given_range_params(range.start, range._end, bufnr)
 
-	if params.range["end"].line == -1 then
-		params.range["end"].line = 0
-	end
+  if params.range["end"].line == -1 then
+    params.range["end"].line = 0
+  end
 
-	if params.range["start"].line == -1 then
-		params.range["start"].line = 0
-	end
+  if params.range["start"].line == -1 then
+    params.range["start"].line = 0
+  end
 
-	return range, params
+  return range, params
 end
 
-local namespace = vim.api.nvim_create_namespace("experimental/inlayHints")
+local namespace = vim.api.nvim_create_namespace "experimental/inlayHints"
 local enabled = nil
 
 local function adapter(result, ctx)
-	local client = vim.tbl_filter(function(c)
-		return c.id == ctx.client_id
-	end, vim.lsp.get_active_clients())[1]
+  local client = vim.tbl_filter(function(c)
+    return c.id == ctx.client_id
+  end, vim.lsp.get_active_clients())[1]
 
-	-- offspec
-	if client.name == "tsserver" and result.inlayHints then
-		result = vim.tbl_map(function(h)
-			h.label = h.text
-			local kind = h.kind
-			if type(kind) == "string" then
-				h.kind = (kind == "Parameter" and 2) or 1
-			end
+  -- offspec
+  if client.name == "tsserver" and result.inlayHints then
+    result = vim.tbl_map(function(h)
+      h.label = h.text
+      local kind = h.kind
+      if type(kind) == "string" then
+        h.kind = (kind == "Parameter" and 2) or 1
+      end
 
-			return h
-		end, result.inlayHints)
-	end
+      return h
+    end, result.inlayHints)
+  end
 
-	return result
+  return result
 end
 
 local function parseHints(result, ctx)
-	local map = {}
-	local only_current_line = config.options.tools.inlay_hints.only_current_line
+  local map = {}
+  local only_current_line = config.options.tools.inlay_hints.only_current_line
 
-	if type(result) ~= "table" then
-		return {}
-	end
+  if type(result) ~= "table" then
+    return {}
+  end
 
-	result = adapter(result, ctx)
+  result = adapter(result, ctx)
 
-	for _, inlayHint in pairs(result) do
-		local range = inlayHint.position
-		local line = tonumber(inlayHint.position.line)
-		local label = inlayHint.label
-		local kind = inlayHint.kind or 1
+  for _, inlayHint in pairs(result) do
+    local range = inlayHint.position
+    local line = tonumber(inlayHint.position.line)
+    local label = inlayHint.label
+    local kind = inlayHint.kind or 1
 
-		local current_line = vim.api.nvim_win_get_cursor(0)[1]
+    local current_line = vim.api.nvim_win_get_cursor(0)[1]
 
-		local function add_line()
-			if not map[line] then
-				---@diagnostic disable-next-line: need-check-nil
-				map[line] = {}
-			end
+    local function add_line()
+      if not map[line] then
+        ---@diagnostic disable-next-line: need-check-nil
+        map[line] = {}
+      end
 
-			table.insert(map[line], { label = label, kind = kind, range = range })
-		end
+      table.insert(map[line], { label = label, kind = kind, range = range })
+    end
 
-		if only_current_line then
-			if line == current_line - 1 then
-				add_line()
-			end
-		else
-			add_line()
-		end
-	end
-	return map
+    if only_current_line then
+      if line == current_line - 1 then
+        add_line()
+      end
+    else
+      add_line()
+    end
+  end
+  return map
 end
 
 local function get_max_len(bufnr, parsed_data)
-	local max_len = -1
+  local max_len = -1
 
-	for line, _ in pairs(parsed_data) do
-		local current_line = vim.api.nvim_buf_get_lines(bufnr, line, line + 1, false)[1]
-		if current_line then
-			local current_line_len = string.len(current_line)
-			max_len = math.max(max_len, current_line_len)
-		end
-	end
+  for line, _ in pairs(parsed_data) do
+    local current_line = vim.api.nvim_buf_get_lines(bufnr, line, line + 1, false)[1]
+    if current_line then
+      local current_line_len = string.len(current_line)
+      max_len = math.max(max_len, current_line_len)
+    end
+  end
 
-	return max_len
+  return max_len
 end
 
 local function handler(err, result, ctx, range)
-	local opts = config.options.tools.inlay_hints
-	if err then
-		if err.message:match("textDocument") then
-			return
-		end
+  local opts = config.options.tools.inlay_hints
+  if err then
+    if err.message:match "textDocument" then
+      return
+    end
 
-		if opts.debug_mode then
-			vim.notify("inlay_hints: " .. err.message, vim.log.levels.ERROR)
-		end
-		return
-	end
+    if opts.debug_mode then
+      vim.notify("inlay_hints: " .. err.message, vim.log.levels.ERROR)
+    end
+    return
+  end
 
-	local bufnr = ctx.bufnr
+  local bufnr = ctx.bufnr
 
-	if vim.api.nvim_get_current_buf() ~= bufnr then
-		return
-	end
+  if vim.api.nvim_get_current_buf() ~= bufnr then
+    return
+  end
 
-	local parsed = parseHints(result, ctx)
+  local parsed = parseHints(result, ctx)
 
-	-- clean it up at first
-	M.clear_inlay_hints(range.start[1], range._end[1])
+  -- clean it up at first
+  M.clear_inlay_hints(range.start[1], range._end[1])
 
-	for line, line_hints in pairs(parsed) do
-		local virt_text = ""
+  for line, line_hints in pairs(parsed) do
+    local virt_text = ""
 
-		local current_line = vim.api.nvim_buf_get_lines(bufnr, line, line + 1, false)[1]
-		if current_line then
-			local current_line_len = string.len(current_line)
+    local current_line = vim.api.nvim_buf_get_lines(bufnr, line, line + 1, false)[1]
+    if current_line then
+      local current_line_len = string.len(current_line)
 
-			local param_labels = {}
-			local type_hints = {}
+      local param_labels = {}
+      local type_hints = {}
 
-			-- segregate parameter hints and other hints
-			for _, hint in ipairs(line_hints) do
-				if hint.kind == 2 then
-					-- label may be a string or InlayHintLabelPart[]
-					-- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#inlayHintLabelPart
-					if type(hint.label) == "table" then
-						local values = vim.tbl_map(function(label_part)
-							return label_part.value
-						end, hint.label)
-						vim.list_extend(param_labels, values)
-					else
-						table.insert(param_labels, hint.label.value)
-					end
-				else
-					table.insert(type_hints, hint)
-				end
-			end
+      -- segregate parameter hints and other hints
+      for _, hint in ipairs(line_hints) do
+        if hint.kind == 2 then
+          -- label may be a string or InlayHintLabelPart[]
+          -- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#inlayHintLabelPart
+          if type(hint.label) == "table" then
+            local values = vim.tbl_map(function(label_part)
+              return label_part.value
+            end, hint.label)
+            vim.list_extend(param_labels, values)
+          else
+            table.insert(param_labels, hint.label.value)
+          end
+        else
+          table.insert(type_hints, hint)
+        end
+      end
 
-			-- show parameter hints inside brackets with commas and a thin arrow
-			if not vim.tbl_isempty(param_labels) and opts.show_parameter_hints then
-				virt_text = virt_text .. opts.parameter_hints_prefix .. "("
-				for i, label in ipairs(param_labels) do
-					virt_text = virt_text .. label:sub(1, -2)
-					if i ~= #param_labels then
-						virt_text = virt_text .. ", "
-					end
-				end
-				virt_text = virt_text .. ") "
-			end
+      -- show parameter hints inside brackets with commas and a thin arrow
+      if not vim.tbl_isempty(param_labels) and opts.show_parameter_hints then
+        virt_text = virt_text .. opts.parameter_hints_prefix .. "("
+        for i, label in ipairs(param_labels) do
+          virt_text = virt_text .. label:sub(1, -2)
+          if i ~= #param_labels then
+            virt_text = virt_text .. ", "
+          end
+        end
+        virt_text = virt_text .. ") "
+      end
 
-			-- show other hints with commas and a prefix
-			if not vim.tbl_isempty(type_hints) then
-				virt_text = virt_text .. opts.type_hints_prefix
-				local process_virtual_text = function(hint, value)
-					local label = value
-					if opts.show_variable_name then
-						local char_start = hint.range.start.character
-						local char_end = hint.range["end"].character
-						local variable_name = string.sub(current_line, char_start + 1, char_end)
+      -- show other hints with commas and a prefix
+      if not vim.tbl_isempty(type_hints) then
+        virt_text = virt_text .. opts.type_hints_prefix
+        local process_virtual_text = function(hint, value)
+          local label = value
+          if opts.show_variable_name then
+            local char_start = hint.range.start.character
+            local char_end = hint.range["end"].character
+            local variable_name = string.sub(current_line, char_start + 1, char_end)
 
-						virt_text = virt_text .. variable_name .. ": " .. label
-					else
-						if opts.other_hints_remove_colon then
-							-- remove ': ' or ':'
-							label = label:match("^:?%s?(.*)$")
-						end
-						if opts.other_hints_remove_colon_end then
-							-- remove ':' after
-							label = label:match("(.*):$")
-						end
-						virt_text = virt_text .. label
-					end
-				end
+            virt_text = virt_text .. variable_name .. ": " .. label
+          else
+            if opts.other_hints_remove_colon then
+              -- remove ': ' or ':'
+              label = label:match "^:?%s?(.*)$"
+            end
+            if opts.other_hints_remove_colon_end then
+              -- remove ':' after
+              label = label:match "(.*):$"
+            end
+            virt_text = virt_text .. label
+          end
+        end
 
-				for i, hint in ipairs(type_hints) do
-					-- label may be a string or InlayHintLabelPart[]
-					-- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#inlayHintLabelPart
-					if type(hint.label) == "table" then
-						for j, label_part in ipairs(hint.label) do
-							process_virtual_text(hint, label_part.value)
-							if j ~= #hint.label then
-								virt_text = virt_text .. ", "
-							end
-						end
-					else
-						process_virtual_text(hint, hint.label)
-					end
+        for i, hint in ipairs(type_hints) do
+          -- label may be a string or InlayHintLabelPart[]
+          -- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#inlayHintLabelPart
+          if type(hint.label) == "table" then
+            for j, label_part in ipairs(hint.label) do
+              process_virtual_text(hint, label_part.value)
+              if j ~= #hint.label then
+                virt_text = virt_text .. ", "
+              end
+            end
+          else
+            process_virtual_text(hint, hint.label)
+          end
 
-					if i ~= #type_hints and not virt_text:match(",%s$") then
-						virt_text = virt_text .. ", "
-					end
-				end
-			end
+          if i ~= #type_hints and not virt_text:match ",%s$" then
+            virt_text = virt_text .. ", "
+          end
+        end
+      end
 
-			if config.options.tools.inlay_hints.right_align then
-				virt_text = virt_text .. string.rep(" ", config.options.tools.inlay_hints.right_align_padding)
-			end
+      if config.options.tools.inlay_hints.right_align then
+        virt_text = virt_text
+          .. string.rep(" ", config.options.tools.inlay_hints.right_align_padding)
+      end
 
-			if config.options.tools.inlay_hints.max_len_align then
-				local max_len = get_max_len(bufnr, parsed)
-				virt_text = string.rep(
-					" ",
-					max_len - current_line_len + config.options.tools.inlay_hints.max_len_align_padding
-				) .. virt_text
-			end
+      if config.options.tools.inlay_hints.max_len_align then
+        local max_len = get_max_len(bufnr, parsed)
+        virt_text = string.rep(
+          " ",
+          max_len - current_line_len + config.options.tools.inlay_hints.max_len_align_padding
+        ) .. virt_text
+      end
 
-			-- set the virtual text if it is not empty
-			if virt_text ~= "" then
-				vim.api.nvim_buf_set_extmark(bufnr, namespace, line, 0, {
-					virt_text_pos = config.options.tools.inlay_hints.right_align and "right_align" or "eol",
-					virt_text = {
-						{ virt_text, config.options.tools.inlay_hints.highlight },
-					},
-					hl_mode = "combine",
-				})
-			end
+      -- set the virtual text if it is not empty
+      if virt_text ~= "" then
+        vim.api.nvim_buf_set_extmark(bufnr, namespace, line, 0, {
+          virt_text_pos = config.options.tools.inlay_hints.right_align and "right_align" or "eol",
+          virt_text = {
+            { virt_text, config.options.tools.inlay_hints.highlight },
+          },
+          hl_mode = "combine",
+        })
+      end
 
-			-- update state
-			enabled = true
-		end
-	end
+      -- update state
+      enabled = true
+    end
+  end
 end
 
 local function handler_with_range(range)
-	return function(err, result, ctx)
-		handler(err, result, ctx, range)
-	end
+  return function(err, result, ctx)
+    handler(err, result, ctx, range)
+  end
 end
 
 function M.toggle_inlay_hints()
-	if enabled then
-		M.clear_inlay_hints()
-	else
-		M.set_inlay_hints()
-	end
-	enabled = not enabled
+  if enabled then
+    M.clear_inlay_hints()
+  else
+    M.set_inlay_hints()
+  end
+  enabled = not enabled
 end
 
 -- function M.disable_inlay_hints()
@@ -307,18 +309,18 @@ end
 -- end
 
 function M.clear_inlay_hints(start, _end)
-	-- clear namespace which clears the virtual text as well
-	vim.api.nvim_buf_clear_namespace(0, namespace, start or 0, _end or -1)
+  -- clear namespace which clears the virtual text as well
+  vim.api.nvim_buf_clear_namespace(0, namespace, start or 0, _end or -1)
 end
 
 -- Sends the request to get the inlay hints and handle them
 ---@param endpoint string | nil
 function M.set_inlay_hints(endpoint)
-	local bufnr = vim.api.nvim_get_current_buf()
-	local range, params = get_params(bufnr)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local range, params = get_params(bufnr)
 
-	endpoint = endpoint or "textDocument/inlayHint"
-	utils.request(bufnr, endpoint, params, handler_with_range(range))
+  endpoint = endpoint or "textDocument/inlayHint"
+  utils.request(bufnr, endpoint, params, handler_with_range(range))
 end
 
 local debounce_ms = 200
--- lua/lsp-inlayhints/init.lua
+++ lua/lsp-inlayhints/init.lua
@@ -1,12 +1,12 @@
 local M = {}
-local c = require("lsp-inlayhints.config")
+local c = require "lsp-inlayhints.config"
 
 M.setup = function(user_config)
-	vim.validate({ user_config = { user_config, "table", true } })
-	c.load(user_config)
+  vim.validate { user_config = { user_config, "table", true } }
+  c.load(user_config)
 end
 
-local inlay_hints = require("lsp-inlayhints.core")
+local inlay_hints = require "lsp-inlayhints.core"
 
 M.inlay_hints = inlay_hints.set_inlay_hints
 M.setup_autocmd = inlay_hints.setup_autocmd
