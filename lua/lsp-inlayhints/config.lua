eac8c4bcb313efa16cc041aaf304f303ef5342ec
lvimuser <109605931+lvimuser@users.noreply.github.com>
2022-07-22 14:54:46 +0000

    chore(config)!: remove 'tools' field

--- lua/lsp-inlayhints/config.lua
+++ lua/lsp-inlayhints/config.lua
@@ -22,21 +22,19 @@ end
 local config = {}
 
 local default_config = {
-  tools = {
-    inlay_hints = {
-      only_current_line = false,
-      only_current_line_autocmd = "CursorHold",
-      show_parameter_hints = true,
-      show_variable_name = false,
-      parameter_hints_prefix = "<- ",
-      type_hints_prefix = "",
-      max_len_align = false,
-      max_len_align_padding = 1,
-      right_align = false,
-      right_align_padding = 7,
-      highlight = "LspInlayHint",
-      debug_mode = false,
-    },
+  inlay_hints = {
+    only_current_line = false,
+    only_current_line_autocmd = "CursorHold",
+    show_parameter_hints = true,
+    show_variable_name = false,
+    parameter_hints_prefix = "<- ",
+    type_hints_prefix = "",
+    max_len_align = false,
+    max_len_align_padding = 1,
+    right_align = false,
+    right_align_padding = 7,
+    highlight = "LspInlayHint",
+    debug_mode = false,
   },
 }
 
